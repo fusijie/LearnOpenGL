1b00872cb7b168a7fe08a59e406023ede7662e09
fusijie <fusijie@vip.qq.com>
2020-04-01 16:10:14 +0800

    init repo.

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by 傅思杰 on 2020/4/1.
+//  Copyright © 2020 傅思杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
