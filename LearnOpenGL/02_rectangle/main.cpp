c814ca7fef52d3409eba84aa949c169334a60dc8
fusijie <fusijie@vip.qq.com>
2020-04-02 17:55:32 +0800

    fixed bug

--- LearnOpenGL/02_rectangle/main.cpp
+++ LearnOpenGL/02_rectangle/main.cpp
@@ -159,6 +159,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
