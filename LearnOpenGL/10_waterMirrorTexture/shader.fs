e2b2f54bca524b61d1ee4d75088c80c11db82277
fusijie <fusijie@vip.qq.com>
2020-04-07 16:42:18 +0800

    update waterMirrorTexture

--- LearnOpenGL/10_waterMirrorTexture/shader.fs
+++ LearnOpenGL/10_waterMirrorTexture/shader.fs
@@ -5,11 +5,12 @@ in vec3 ourColor;
 in vec2 texCoord;
 
 uniform sampler2D texture1;
+uniform float time;
 
 void main()
 {
     if (texCoord.y < 0.5)
         FragColor = texture(texture1, texCoord);
     else
-        FragColor = texture(texture1, vec2(texCoord.x, 1 - texCoord.y));
+        FragColor = texture(texture1, vec2(texCoord.x + sin(texCoord.y * 60.0 + time * 2.0) / 200.0, 1.0 - texCoord.y)) * vec4(0.7, 0.7, 1.0, 1.0);
 }
