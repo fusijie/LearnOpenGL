9dd168fd9d5ade20de045fffed9a56554ef84367
fusijie <fusijie@vip.qq.com>
2020-04-09 18:12:43 +0800

    add material

--- LearnOpenGL/20_light/shader.fs
+++ LearnOpenGL/20_light/shader.fs
@@ -12,14 +12,17 @@ uniform vec3 viewPos;
 
 void main()
 {
+    //ambient
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
     
+    //diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
     
+    //specular
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
--- LearnOpenGL/Shader.h
+++ LearnOpenGL/Shader.h
@@ -108,6 +108,7 @@ public:
     {
         glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
     }
+    // ------------------------------------------------------------------------
     void setVec3f(const std::string &name, const float* value) const
     {
         glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, value);
