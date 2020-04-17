edf470f35e9f9f8db3e13ef2f2736bc0f5748b8b
fusijie <fusijie@vip.qq.com>
2020-04-17 18:31:36 +0800

    add normal visualization.

---
+++ LearnOpenGL/44_normalVisualization/main.cpp
@@ -0,0 +1,155 @@
+//
+//  main.cpp
+//
+//  Created by 傅思杰 on 2020/4/3.
+//  Copyright © 2020 傅思杰. All rights reserved.
+//
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "common/Utils.h"
+#include "common/Shader.h"
+#include "common/Camera.h"
+#include "common/Model.h"
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+void myProcessInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+int main()
+{
+    // init glfw and create window
+    GLFWwindow* window = initGLFW("NormalVisualization");
+    if (!window)
+    {
+        return -1;
+    }
+    
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    // configure global opengl state
+    glEnable(GL_DEPTH_TEST);
+    
+    // build and compile our shader program
+    Shader ourShader("shader.vs", "shader.fs");
+    Shader normalShader("normal.vs", "normal.gs", "normal.fs");
+    
+    // load model
+    Model ourModel("nanosuit/nanosuit.obj");
+    
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
+        // input
+        myProcessInput(window);
+        
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        // be sure to activate shader when setting uniforms/drawing objects
+        ourShader.use();
+        
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));    // it's a bit too big for our scene, so scale it down
+
+        // render the loaded model
+        ourShader.setMatrix4fv("model", glm::value_ptr(model));
+        ourShader.setMatrix4fv("projection", glm::value_ptr(projection));
+        ourShader.setMatrix4fv("view", glm::value_ptr(view));
+        
+        ourModel.Draw(ourShader);
+        
+        // then draw model with normal visualizing geometry shader
+        normalShader.use();
+        normalShader.setMatrix4fv("projection", glm::value_ptr(projection));
+        normalShader.setMatrix4fv("view", glm::value_ptr(view));
+        normalShader.setMatrix4fv("model", glm::value_ptr(model));
+
+        ourModel.Draw(normalShader);
+        
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    // optional: de-allocate all resources once they've outlived their purpose:
+    
+    ourShader.clear();
+    
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void myProcessInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    
+    lastX = xpos;
+    lastY = ypos;
+    
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
